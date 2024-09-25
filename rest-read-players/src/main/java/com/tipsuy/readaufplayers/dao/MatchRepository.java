93e2daf3c7af04e14881966d3a22a5c26213a96d
dani77uy <dinguel@adinet.com.uy>
2024-09-25 07:52:30 -0300

    fix sin fix

--- rest-read-players/src/main/java/com/tipsuy/readaufplayers/dao/MatchRepository.java
+++ rest-read-players/src/main/java/com/tipsuy/readaufplayers/dao/MatchRepository.java
@@ -1,14 +1,10 @@
 package com.tipsuy.readaufplayers.dao;
 
-import org.springframework.data.mongodb.repository.MongoRepository;
-import org.springframework.data.mongodb.repository.Query;
-import org.springframework.stereotype.Repository;
-
 import com.tipsuy.readaufplayers.domain.Match;
+import org.springframework.data.mongodb.repository.MongoRepository;
+import org.springframework.stereotype.Repository;
 
 @Repository
 public interface MatchRepository extends MongoRepository<Match, Long> {
 
-   @Query(value = "{ '$or': [ { 'homeTeam': ?0 }, { 'awayTeam': ?0 } ], {''} }", sort = "{'matchDateTime': 1}")
-   Iterable<Match> findByMatchesFromTeam(Short teamId, Short seasonId);
 }
--- rest-read-players/src/main/java/com/tipsuy/readaufplayers/service/MatchService.java
+++ rest-read-players/src/main/java/com/tipsuy/readaufplayers/service/MatchService.java
@@ -1,10 +1,5 @@
 package com.tipsuy.readaufplayers.service;
 
-import java.util.ArrayList;
-import java.util.List;
-
-import org.springframework.stereotype.Service;
-
 import com.tipsuy.readaufplayers.dao.MatchRepository;
 import com.tipsuy.readaufplayers.dao.SeasonRepository;
 import com.tipsuy.readaufplayers.dao.template.MatchDAO;
@@ -14,8 +9,12 @@ import com.tipsuy.readaufplayers.domain.MatchPlayer;
 import com.tipsuy.readaufplayers.domain.PlayerExecution;
 import com.tipsuy.readaufplayers.domain.dto.MatchDTO;
 import com.tipsuy.readaufplayers.util.DateUtil;
-
+import java.time.OffsetDateTime;
+import java.util.ArrayList;
+import java.util.List;
 import lombok.RequiredArgsConstructor;
+import org.apache.commons.lang3.math.NumberUtils;
+import org.springframework.stereotype.Service;
 
 @RequiredArgsConstructor
 @Service
@@ -44,26 +43,43 @@ public class MatchService {
    }
 
    public List<Match> addMatchesPlayers(final short seasonId, final byte matchDay) {
+      // leo los partidos de la jornada por temporada
       final var matchesList = matchDAO.findMatchesBySeasonAndMatchDay(seasonId, matchDay);
+      // creo una lista vacía de partidos donde se adicionarán jugadores
       final var matchesUpdated = new ArrayList<Match>(matchesList.size());
       matchesList.forEach(match -> {
+         // recorro los partidos de la jornada
          final var matchDateTime = match.getMatchDateTime();
          final var homeTeam = match.getHomeTeam();
          final var awayTeam = match.getAwayTeam();
-         final Match lastMatchOfHomeTeam = getTheLatestMatchOfTeam(homeTeam);
-         final Match lastMatchOfAwayTeam = getTheLatestMatchOfTeam(awayTeam);
-         final List<Execution> executionsHomeTeam = getAllExecutionsBetweenDates(matchDateTime, lastMatchOfHomeTeam.getMatchDateTime());
-         final List<Execution> executionsAwayTeam = getAllExecutionsBetweenDates(matchDateTime, lastMatchOfAwayTeam.getMatchDateTime());
-         final List<PlayerExecution> playersExecutionHomeTeam = getPlayerExecutions(executionsHomeTeam);
-         final List<PlayerExecution> playersExecutionAwayTeam = getPlayerExecutions(executionsAwayTeam);
-         match.addMatchPlayers(calculateMatchPlayers(playersExecutionHomeTeam, playersExecutionAwayTeam));
-         matchesUpdated.add(matchDAO.addMatchPlayers(match.getMatchId(), match.getMatchPlayers()));
+         if (matchDay == NumberUtils.BYTE_ONE) {
+            //TODO
+         } else {
+            final List<Match> lastMatchDayMatches = matchDAO.findMatchesBySeasonAndMatchDay(seasonId, (byte) (matchDay - NumberUtils.BYTE_ONE));
+            // busco cuándo fue el último partido del local, anterior al partido actual en esta temporada
+            final var lastMatchDateTimeOfHomeTeam = lastMatchDayMatches.stream().filter(x -> x.getHomeTeam().equals(homeTeam)).findFirst().map(Match::getMatchDateTime).orElseThrow();
+            // busco cuándo fue el último partido del visitante, anterior al partido actual en esta temporada
+            final var lastMatchDateTimeOfAwayTeam = lastMatchDayMatches.stream().filter(x -> x.getHomeTeam().equals(awayTeam)).findFirst().map(Match::getMatchDateTime).orElseThrow();
+            // busco la lista de ejecuciones entre el partido actual y el anterior para el local
+            final List<Execution> executionsHomeTeam = getAllExecutionsBetweenDates(matchDateTime, lastMatchDateTimeOfHomeTeam);
+            // busco la lista de ejecuciones entre el partido actual y el anterior para el visitante
+            final List<Execution> executionsAwayTeam = getAllExecutionsBetweenDates(matchDateTime, lastMatchDateTimeOfAwayTeam);
+            // busco la info de jugadores en esas ejecuciones, para el local
+            final List<PlayerExecution> playersExecutionHomeTeam = getPlayerExecutions(executionsHomeTeam);
+            // busco la info de jugadores en esas ejecuciones, para el visitante
+            final List<PlayerExecution> playersExecutionAwayTeam = getPlayerExecutions(executionsAwayTeam);
+            // calculo la diferencia de partidos, goles y minutos a cada jugador que estuvo involucrado en las ejecuciones
+            match.addMatchPlayers(calculateMatchPlayers(playersExecutionHomeTeam, playersExecutionAwayTeam));
+            // actualizo la base de datos con los jugadores del partido
+            matchesUpdated.add(matchDAO.addMatchPlayers(match.getMatchId(), match.getMatchPlayers()));
+         }
+
       });
       return matchesUpdated;
    }
 
-   private Match getTheLatestMatchOfTeam(final Short teamId) {
-      final Iterable<Match> latestMatches = matchRepository.findByMatchesFromTeam(teamId);
+   private List<Execution> getAllExecutionsBetweenDates(final OffsetDateTime matchDateTime, final OffsetDateTime lastMatchDateTimeOfHomeTeam) {
+
    }
 
    private Match addMatchPlayers(final long matchId, final List<MatchPlayer> matchPlayers) {
